6d7bfd41a6a19267dee3b78a3278970b9ee090ad
EmmaC <c.emma2112@gmail.com>
2023-07-27 19:01:38 +0000

    remove default secret key & get key from env.py

--- brick_by_brick/settings.py
+++ brick_by_brick/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&)l-s%8mgs$4)9%^6book63l(%z$3eg04ysv$!)*4u^9_mb5dv'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
