6140f086e022be7911a2f01b56910fb3122c98d1
EmmaC <c.emma2112@gmail.com>
2023-07-27 18:49:59 +0000

    initial project setup

---
+++ brick_by_brick/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for brick_by_brick project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brick_by_brick.settings')
+
+application = get_asgi_application()
