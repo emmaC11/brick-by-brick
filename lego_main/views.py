5134a750d898fa54164b0555a72628ee87234c00
EmmaC <c.emma2112@gmail.com>
2023-08-05 16:06:09 +0000

    add a view function that displays list of Lego sets/products on the product list page

--- lego_main/views.py
+++ lego_main/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import LegoSet
 
 # Create your views here.
 
 
 class HomeView(generic.TemplateView):
     template_name = 'home.html'
+
+
+def product_list(request):
+    context = {
+        'lego_sets': LegoSet.objects.all(),
+    }
+    return render(request, 'product_list.html')
