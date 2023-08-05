c2049947f367cc055c584807e403256ad383dcbb
EmmaC <c.emma2112@gmail.com>
2023-08-05 14:52:17 +0000

    add fields to LegoSet model

--- lego_main/models.py
+++ lego_main/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class LegoSet(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    piece_count = models.PositiveIntegerField()
+    item_number = models.PositiveIntegerField()
+    ages = models.PositiveIntegerField()
+    minifigures = models.PositiveIntegerField()
+    stock_quantity = models.PositiveIntegerField(default=0)
+    is_available = models.BooleanField(default=True)
+    image = CloudinaryField('image', default='placeholder')
+    theme = models.ForeignKey(LegoSetTheme, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
     pass
 
 
