97f9f2aa4143da6a6d480c2e8372923429d5eb20
EmmaC <c.emma2112@gmail.com>
2023-08-05 14:42:48 +0000

    define models

--- lego_main/models.py
+++ lego_main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class LegoSet(models.Model):
+    pass
+
+
+class LegoSetTheme(models.Model):
+    pass
+
+
+# when a lego set is added to the cart it becomes an order item
+class OrderItem(models.Model):
+    pass
+
+
+class Order(models.Model):
+    pass
