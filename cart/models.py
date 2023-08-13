6d6c36bded380c2de0b4fbd43d6c69b765f7dc4c
EmmaC <c.emma2112@gmail.com>
2023-08-13 13:29:24 +0000

    define address type field that uses choices tuple

--- cart/models.py
+++ cart/models.py
@@ -4,12 +4,18 @@ from cloudinary.models import CloudinaryField
 
 
 class Address(models.Model):
+    ADDRESS_CHOICES = (
+        ('B', 'Billing'),
+        ('S', 'Shipping'),
+    )
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     address_line_1 = models.CharField(max_length=200)
     address_line_2 = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
     postal_code = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
+    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
 
     def __str__(self):
         return self.address_line_1
