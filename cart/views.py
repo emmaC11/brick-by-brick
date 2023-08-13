313db5163cf563ab270f6e237348fe1a3cc78275
EmmaC <c.emma2112@gmail.com>
2023-08-13 14:56:42 +0000

    use LegoSet model in queryset for ProductListView

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
+from cart.models import LegoSet
+
 
 class ProductListView(generic.ListView):
-    
+    queryset = LegoSet.objects.all()
+
