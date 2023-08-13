ee1007cf98c31c5d169af8c075294db3bbffef97
EmmaC <c.emma2112@gmail.com>
2023-08-13 13:53:06 +0000

    uncomment cart path & import includes module

--- lego_main/urls.py
+++ lego_main/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from lego_main import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('product_list/', views.product_list, name='product_list'),
     path('marketing/', views.MarketingFormView.as_view(), name='marketing'),
-    # path('cart/', include('cart.urls', namespace='cart')),
+    path('cart/', include('cart.urls', namespace='cart')),
 
 ]
