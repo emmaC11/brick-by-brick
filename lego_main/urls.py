1619f4a9cae31cbbbac07f0a8b0a9feac366b728
EmmaC <c.emma2112@gmail.com>
2023-08-13 12:51:34 +0000

    add path to cart.urls in urls.py in lego_main directory

--- lego_main/urls.py
+++ lego_main/urls.py
@@ -4,6 +4,7 @@ from lego_main import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('product_list/', views.product_list, name='product_list'),
-    path('marketing/', views.MarketingFormView.as_view(), name='marketing')
+    path('marketing/', views.MarketingFormView.as_view(), name='marketing'),
+    path('cart/', include('cart.urls', namespace='cart')),
 
 ]
