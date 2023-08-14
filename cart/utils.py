b91514717bdb6f6d176282b2395e641e13490dff
EmmaC <c.emma2112@gmail.com>
2023-08-14 19:35:29 +0000

    import order model & create instance of this class

--- cart/utils.py
+++ cart/utils.py
@@ -1,5 +1,12 @@
+from cart.models import Order
+
+
 def get_or_set_order_session(request):
     # check if order id is in session
     order_id = request.session.get('order_id', None)
+
+    # if order id is not in session, we want to create an order
+    if order_id is None:
+        order = Order()
 
     
