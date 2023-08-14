df211f02a6358dc6266dcc48803421557bea50c4
EmmaC <c.emma2112@gmail.com>
2023-08-14 19:47:16 +0000

    define get_object function that uses LegoSet object via slug

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404    
 from django.views import generic
 from cart.models import LegoSet
 from .utils import get_or_set_order_session
@@ -11,5 +11,8 @@ class ProductListView(generic.ListView):
 
 class LegoSetDetailView(generic.FormView):
     template_name = 'cart/product.html'
-    queryset = LegoSet.objects.all()
+    # queryset = LegoSet.objects.all()
+
+    def get_object(self):
+        return get_object_or_404(LegoSet, slug=self.kwargs['slug'])
 
