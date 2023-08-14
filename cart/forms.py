ecc285b3d4933bcf2819c3e988ed215b27845d18
EmmaC <c.emma2112@gmail.com>
2023-08-14 19:59:32 +0000

    define meta class within AddToCart form using OrderItem as model

--- cart/forms.py
+++ cart/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from cart.models import OrderItem
 
+
 class AddToCartForm(forms.ModelForm):
-    
+
+    class Meta:
+        model = OrderItem
+        fields = ['quantity']
