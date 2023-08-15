6b673ecbeffa370c5758c808ffc832aac25c7991
EmmaC <c.emma2112@gmail.com>
2023-08-15 06:49:04 +0000

    update path tos have '/shop' in url & add path for CartView

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,7 @@ from cart import views
 app_name = 'cart'
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product_list'),
-    path('<slug>/', views.LegoSetDetailView.as_view(), name='product_detail'),
+    path('', views.CartView.as_view(), name='cart_summary'),
+    path('shop/', views.ProductListView.as_view(), name='product_list'),
+    path('shop/<slug>/', views.LegoSetDetailView.as_view(), name='product_detail'),
 ]
