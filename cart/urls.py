79c61a1c54dd6ba4bad1d04fbd528aef9807b1aa
EmmaC <c.emma2112@gmail.com>
2023-08-19 21:31:59 +0000

    add url path via PK for DecrementLegoSetQuantityView

--- cart/urls.py
+++ cart/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('shop/', views.ProductListView.as_view(), name='product_list'),
     path('shop/<slug>/', views.LegoSetDetailView.as_view(), name='product_detail'),
     path('increase_quantity/<pk>/', views.IncrementLegoSetQuantityView.as_view(), name='increase_quantity'),
+    path('decrease_quantity/<pk>/', views.DecrementLegoSetQuantityView.as_view(), name='decrease_quantity'),
 ]
