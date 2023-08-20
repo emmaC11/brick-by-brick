762bb77c863ef2550d49d1f190a161c25447f2c4
EmmaC <c.emma2112@gmail.com>
2023-08-20 10:55:26 +0000

    import get_or_set_order_session & assign order var

--- cart/templatetags/cart_template_tags.py
+++ cart/templatetags/cart_template_tags.py
@@ -1,8 +1,10 @@
 from django import template
+from cart.utils import get_or_set_order_session
 
 register = template.Library()
 
 
 @register.filter
 def cart_item_count(request):
-    
+    # get order from the session & count the number of OrderItems quantity & return that num
+    order = get_or_set_order_session(request)
