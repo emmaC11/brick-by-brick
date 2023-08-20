733339227e56948e11e97fdc686d91829b01dcf5
EmmaC <c.emma2112@gmail.com>
2023-08-20 17:21:03 +0000

    add list display of Address model data

--- cart/admin.py
+++ cart/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from .models import LegoSetTheme, LegoSet, OrderItem, Order, Address
 
-# Register your models here.
+
+class AddressAdminDisplay(admin.ModelAdmin):
+    list_display = [
+        'address_line_1',
+        'address_line_2',
+        'city',
+        'postal_code',
+        'address_type',
+    ]
+
+
 admin.site.register(LegoSetTheme)
 admin.site.register(LegoSet)
 admin.site.register(OrderItem)
 admin.site.register(Order)
-admin.site.register(Address)
+admin.site.register(Address, AddressAdminDisplay)
 
 # Register your models here.
