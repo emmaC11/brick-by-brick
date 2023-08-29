9c58e71989edb028c3f995b91f3c53c95f289744
EmmaC <c.emma2112@gmail.com>
2023-08-29 20:01:09 +0000

    add 2 blank lines between class definitions

--- cart/forms.py
+++ cart/forms.py
@@ -11,6 +11,7 @@ class LegoThemeFilterForm(forms.Form):
     theme_choices = [(theme.id, theme.theme_name) for theme in LegoSetTheme.objects.all()]
     selected_theme = forms.ChoiceField(choices=[('', 'All Themes')] + theme_choices, required=False)
 
+
 class AddToCartForm(forms.ModelForm):
 
     class Meta:
