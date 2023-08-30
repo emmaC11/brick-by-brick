f30d16c603941d87df8c99deff0429199466dc4a
EmmaC <c.emma2112@gmail.com>
2023-08-30 20:20:04 +0000

    update get calls to match config vars in heroku

--- brick_by_brick/settings.py
+++ brick_by_brick/settings.py
@@ -77,8 +77,8 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 SITE_ID = 1
 ACCOUNT_EMAIL_VERIFICATION = 'none'
