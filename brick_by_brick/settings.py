464fc0d8c8715d350a8a3efd9b974a1ceef5fc7c
EmmaC <c.emma2112@gmail.com>
2023-08-30 20:18:28 +0000

    add configuration for using smtp in settings.py

--- brick_by_brick/settings.py
+++ brick_by_brick/settings.py
@@ -65,10 +65,20 @@ INSTALLED_APPS = [
     'brick_by_brick'
 ]
 
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-NOTIFY_EMAIL = os.environ.get("NOTIFY_EMAIL")
+# DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+ # NOTIFY_EMAIL = os.environ.get("NOTIFY_EMAIL")
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'brickbybrick@console.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 SITE_ID = 1
 ACCOUNT_EMAIL_VERIFICATION = 'none'
