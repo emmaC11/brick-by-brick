33adbedaa41d4d0ee2fe896e8640b4cad0658667
EmmaC <c.emma2112@gmail.com>
2023-10-15 15:03:39 +0000

    move ProductListView class to views.py in products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from products.models import LegoSet, LegoSetTheme
 
-# Create your views here.
+class ProductListView(generic.ListView):
+    template_name = 'cart/product_list.html'
+    queryset = LegoSet.objects.all()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        theme_id = self.request.GET.get('selected_theme')
+        if theme_id:
+            queryset = queryset.filter(theme=theme_id)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['lego_theme_filter_form'] = LegoThemeFilterForm()
+
+        # get selected theme's description and pass it to the context
+        selected_theme_id = self.request.GET.get('selected_theme')
+        if selected_theme_id:
+            selected_theme = LegoSetTheme.objects.get(pk=selected_theme_id)
+            context['selected_theme_description'] = (
+                selected_theme.theme_description
+            )
+            context['selected_theme_name'] = selected_theme.theme_name
+        return context
+
+
