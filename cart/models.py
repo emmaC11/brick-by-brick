015b355967c50b3fb4b9c7cb395c1cfd6e1ff7bc
EmmaC <c.emma2112@gmail.com>
2023-10-15 14:49:42 +0000

    move LegoSetTheme & LegoSet models in product app

--- cart/models.py
+++ cart/models.py
@@ -24,37 +24,37 @@ class Address(models.Model):
         verbose_name_plural = 'Addresses'
 
 
-class LegoSetTheme(models.Model):
-    theme_name = models.CharField(max_length=200)
-    theme_description = models.CharField(max_length=500)
+# class LegoSetTheme(models.Model):
+#     theme_name = models.CharField(max_length=200)
+#     theme_description = models.CharField(max_length=500)
 
-    def __str__(self):
-        return self.theme_name
+#     def __str__(self):
+#         return self.theme_name
 
 
-class LegoSet(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField()
-    price = models.IntegerField(default=0)
-    piece_count = models.PositiveIntegerField()
-    item_number = models.PositiveIntegerField()
-    ages = models.PositiveIntegerField()
-    minifigures = models.PositiveIntegerField()
-    stock_quantity = models.PositiveIntegerField(default=0)
-    is_available = models.BooleanField(default=True)
-    image = CloudinaryField('image', default='placeholder')
-    theme = models.ForeignKey(LegoSetTheme, on_delete=models.CASCADE)
+# class LegoSet(models.Model):
+#     name = models.CharField(max_length=200)
+#     slug = models.SlugField()
+#     price = models.IntegerField(default=0)
+#     piece_count = models.PositiveIntegerField()
+#     item_number = models.PositiveIntegerField()
+#     ages = models.PositiveIntegerField()
+#     minifigures = models.PositiveIntegerField()
+#     stock_quantity = models.PositiveIntegerField(default=0)
+#     is_available = models.BooleanField(default=True)
+#     image = CloudinaryField('image', default='placeholder')
+#     theme = models.ForeignKey(LegoSetTheme, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
-    pass
+#     def __str__(self):
+#         return self.name
+#     pass
 
-    def get_legoset(self):
-        return reverse('cart:product_detail', args=[self.slug])
+#     def get_legoset(self):
+#         return reverse('cart:product_detail', args=[self.slug])
 
-    def get_legoset_price(self):
-        # divide by 100 to get the price in euro/chosen currency
-        return "{:.2f}".format(self.price/100)
+#     def get_legoset_price(self):
+#         # divide by 100 to get the price in euro/chosen currency
+#         return "{:.2f}".format(self.price/100)
 
 
 # when a lego set is added to the cart it becomes an order item
