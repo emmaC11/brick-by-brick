683238845981fa3404c2065cd062c2997906f32c
EmmaC <c.emma2112@gmail.com>
2023-10-15 14:51:47 +0000

    move Address model into user_details app

--- cart/models.py
+++ cart/models.py
@@ -4,24 +4,24 @@ from django.shortcuts import reverse
 from cloudinary.models import CloudinaryField
 
 
-class Address(models.Model):
-    ADDRESS_CHOICES = (
-        ('B', 'Billing'),
-        ('S', 'Shipping'),
-    )
+# class Address(models.Model):
+#     ADDRESS_CHOICES = (
+#         ('B', 'Billing'),
+#         ('S', 'Shipping'),
+#     )
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    address_line_1 = models.CharField(max_length=200)
-    address_line_2 = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
-    postal_code = models.CharField(max_length=200)
-    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     address_line_1 = models.CharField(max_length=200)
+#     address_line_2 = models.CharField(max_length=200)
+#     city = models.CharField(max_length=200)
+#     postal_code = models.CharField(max_length=200)
+#     address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
 
-    def __str__(self):
-        return self.address_line_1
+#     def __str__(self):
+#         return self.address_line_1
 
-    class Meta:
-        verbose_name_plural = 'Addresses'
+#     class Meta:
+#         verbose_name_plural = 'Addresses'
 
 
 # class LegoSetTheme(models.Model):
