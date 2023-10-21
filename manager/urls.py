da32c42ef567bc2a96a00cae64e6314be4dfe00b
EmmaC <c.emma2112@gmail.com>
2023-10-21 21:02:42 +0000

    add update path to manager urls.py

--- manager/urls.py
+++ manager/urls.py
@@ -5,7 +5,8 @@ app_name = 'manager'
 
 urlpatterns = [
     path('create/', views.CreateLegoSetView.as_view(), name='legoset_create'),
+    path('update/<pk>/', views.UpdateLegoSetView.as_view(),
+         name='legoset_update'),
     path('delete/<pk>/', views.DeleteLegoSetView.as_view(),
          name='legoset_delete')
- 
 ]
