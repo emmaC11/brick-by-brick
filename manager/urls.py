e6b714633a6180a42655485fb75baf969c2690b3
EmmaC <c.emma2112@gmail.com>
2023-10-21 17:24:31 +0000

    replace dash with underscore to keep naming conventions consistent

--- manager/urls.py
+++ manager/urls.py
@@ -4,7 +4,7 @@ from manager import views
 app_name = 'manager'
 
 urlpatterns = [
-    path('create/', views.CreateLegoSetView.as_view(), name='legoset-create'),
+    path('create/', views.CreateLegoSetView.as_view(), name='legoset_create'),
     path('delete/<pk>/', views.DeleteLegoSetView.as_view(),
          name='legoset_delete')
  
