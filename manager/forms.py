2c676622795732963f634ab59abcf125e525c766
EmmaC <c.emma2112@gmail.com>
2023-10-21 16:05:37 +0000

    define model & fields in form class

--- manager/forms.py
+++ manager/forms.py
@@ -3,3 +3,7 @@ from cart.models import LegoSet
 
 
 class LegoSetForm(forms.ModelForm):
+    class Meta:
+        model = LegoSet
+        fields = ['name', 'price', 'piece_count', 'ages', 'minifigures',
+                  'stock_quantity', 'image', 'theme']
