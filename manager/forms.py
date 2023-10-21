bc8316aa94914ecd3acb96a6538e9d40ccbd5482
EmmaC <c.emma2112@gmail.com>
2023-10-21 20:30:28 +0000

    add item number to form fields

--- manager/forms.py
+++ manager/forms.py
@@ -5,5 +5,5 @@ from cart.models import LegoSet
 class LegoSetForm(forms.ModelForm):
     class Meta:
         model = LegoSet
-        fields = ['name', 'price', 'piece_count', 'ages', 'minifigures',
+        fields = ['name', 'price', 'piece_count', ' item_number', 'ages', 'minifigures',
                   'stock_quantity', 'image', 'theme']
