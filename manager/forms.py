a55248ec0504931d02d62d46ea77f28b07658f82
EmmaC <c.emma2112@gmail.com>
2023-10-21 20:43:17 +0000

    add slug field to LegoSetForm

--- manager/forms.py
+++ manager/forms.py
@@ -5,5 +5,5 @@ from cart.models import LegoSet
 class LegoSetForm(forms.ModelForm):
     class Meta:
         model = LegoSet
-        fields = ['name', 'price', 'piece_count', ' item_number', 'ages', 'minifigures',
-                  'stock_quantity', 'image', 'theme']
+        fields = ['name', 'slug', 'price', 'piece_count', 'item_number',
+                  'ages', 'minifigures', 'stock_quantity', 'image', 'theme']
