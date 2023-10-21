30e7bd3bec3faf821358373cc6b94f58a486d4da
EmmaC <c.emma2112@gmail.com>
2023-10-21 14:40:33 +0000

    creath path to delete view using pk as param

--- manager/urls.py
+++ manager/urls.py
@@ -5,5 +5,7 @@ app_name = 'manager'
 
 urlpatterns = [
     path('create/', views.CreateLegoSetView.as_view(), name='legoset-create'),
+    path('delete/<pk>/', views.DeleteLegoSetView.as_view(),
+         name='legoset_delete')
  
 ]
